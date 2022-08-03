0763b562517be3ea5cbc37b06d8135adca91415d
胡磊 <hulei@jushanghui.com>
2022-08-03 10:11:15 +0800

    删除初始化项目中冗余的部分

--- maven-springboot/src/main/java/com/itheima/MavenSpringbootApplication.java
+++ maven-springboot/src/main/java/com/itheima/MavenSpringbootApplication.java
@@ -2,12 +2,19 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MavenSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenSpringbootApplication.class, args);
 	}
 
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello Spring";
+	}
 }
