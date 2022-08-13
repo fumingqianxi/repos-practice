aa26216dc0c2a89aaf34a2038efae92e50b44927
胡磊 <hulei@jushanghui.com>
2022-08-13 12:35:51 +0800

    极客时间常见错误100例03线程池练习

--- maven-springboot/src/test/java/com/itheima/MavenSpringbootApplicationTests.java
+++ maven-springboot/src/test/java/com/itheima/MavenSpringbootApplicationTests.java
@@ -1,13 +1,18 @@
 package com.itheima;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MavenSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+    String str = IntStream.rangeClosed(1, 100).mapToObj(__ -> "a").collect(Collectors.joining(""));
+    String str2 = IntStream.rangeClosed(1, 100).mapToObj(i -> "a").collect(Collectors.joining(""));
+    System.out.println(str);
+    System.out.println(str2);
+  }
 }
