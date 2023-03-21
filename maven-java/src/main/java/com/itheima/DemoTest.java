2081c645aeeb0c8467b30809d1b48ac1a8f9acaf
胡磊 <hulei@jushanghui.com>
2023-03-21 13:25:48 +0800

    测试转义字符和时区问题

--- maven-java/src/main/java/com/itheima/DemoTest.java
+++ maven-java/src/main/java/com/itheima/DemoTest.java
@@ -1,37 +1,35 @@
 package com.itheima;
 
 import java.text.ParseException;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class DemoTest {
   public static void main(String[] args) throws ParseException {
-    //        byte[] input = new byte[] { (byte) 0xe4, (byte) 0xb8, (byte) 0xad };
-    //        String b64encoded = Base64.getEncoder().encodeToString(input);
-    //        System.out.println(b64encoded);
-    //        System.out.println("===================");
-    //        byte[] output = Base64.getDecoder().decode("5Lit");
-    //        System.out.println(Arrays.toString(output)); // [-28, -72, -83]
-    //        System.out.println(Integer.toHexString("hello".hashCode()));
+    method03();
+  }
 
-    Calendar calendar = Calendar.getInstance();
-    //        calendar.set(2022, Calendar.DECEMBER, 29, 3, 0);
-    int day = calendar.get(Calendar.DATE);
-    int hour = calendar.get(Calendar.HOUR_OF_DAY);
-    System.out.println("day: " + day);
-    System.out.println("hour: " + hour);
+  public static void method01() {
+    String a = null;
+    System.out.println(a.length());
+  }
 
-    Date date = new Date();
-    System.out.println(date.getYear());
-    System.out.println(date.getDate());
-    System.out.println(date.getHours());
-    System.out.println(date.toString());
-    System.out.println(date.toGMTString());
-    System.out.println(date.toLocaleString());
+  /**
+   * 替换字符串中转义字符.
+   */
+  public static void method02() {
+    String a = "\\p";
+    System.out.println(a.replaceAll("\\\\", "\\\\\\\\"));
+  }
+
+  /**
+   * 测试LocalDateTime和ZonedDateTime关于时区的问题
+   */
+  public static void method03() {
+    LocalDateTime localDateTime = LocalDateTime.now();
+    System.out.println(localDateTime);
+    ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("Asia/Shanghai"));
+    System.out.println(zonedDateTime);
   }
 }
